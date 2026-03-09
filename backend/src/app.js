8433c5a5b3e4de21ea1a2422a0e06a4c0f16a337
Adarsh Prajapati <code.adarsh121@gmail.com>
2026-03-09 09:22:22 +0530

    adding axios

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-const cookieParser = require('cookie-parser')   
+const cookieParser = require('cookie-parser') 
+const axios = require('axios')  
 
 // Create Express app
 const app = express()
